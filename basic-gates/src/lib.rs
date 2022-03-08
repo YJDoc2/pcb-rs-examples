afae3323e049764ed456bb1f742f20b87eb04ccc
Yashodhan Joshi <yjdoc2@gmail.com>
2022-03-08 08:30:24 +0530

    Add multi-input and or gates for CLA Adder

--- basic-gates/src/lib.rs
+++ basic-gates/src/lib.rs
@@ -61,3 +61,123 @@ impl pcb_rs::Chip for XorGate {
         self.out = self.in1 ^ self.in2;
     }
 }
+
+#[derive(Chip, Default)]
+pub struct And3 {
+    #[pin(input)]
+    pub in1: bool,
+    #[pin(input)]
+    pub in2: bool,
+    #[pin(input)]
+    pub in3: bool,
+    #[pin(output)]
+    pub out: bool,
+}
+
+impl pcb_rs::Chip for And3 {
+    fn tick(&mut self) {
+        self.out = self.in1 && self.in2 && self.in3;
+    }
+}
+
+#[derive(Chip, Default)]
+pub struct And4 {
+    #[pin(input)]
+    pub in1: bool,
+    #[pin(input)]
+    pub in2: bool,
+    #[pin(input)]
+    pub in3: bool,
+    #[pin(input)]
+    pub in4: bool,
+    #[pin(output)]
+    pub out: bool,
+}
+
+impl pcb_rs::Chip for And4 {
+    fn tick(&mut self) {
+        self.out = self.in1 && self.in2 && self.in3 && self.in4;
+    }
+}
+
+#[derive(Chip, Default)]
+pub struct And5 {
+    #[pin(input)]
+    pub in1: bool,
+    #[pin(input)]
+    pub in2: bool,
+    #[pin(input)]
+    pub in3: bool,
+    #[pin(input)]
+    pub in4: bool,
+    #[pin(input)]
+    pub in5: bool,
+    #[pin(output)]
+    pub out: bool,
+}
+
+impl pcb_rs::Chip for And5 {
+    fn tick(&mut self) {
+        self.out = self.in1 && self.in2 && self.in3 && self.in4 && self.in5;
+    }
+}
+
+#[derive(Chip, Default)]
+pub struct Or3 {
+    #[pin(input)]
+    pub in1: bool,
+    #[pin(input)]
+    pub in2: bool,
+    #[pin(input)]
+    pub in3: bool,
+    #[pin(output)]
+    pub out: bool,
+}
+
+impl pcb_rs::Chip for Or3 {
+    fn tick(&mut self) {
+        self.out = self.in1 || self.in2 || self.in3;
+    }
+}
+
+#[derive(Chip, Default)]
+pub struct Or4 {
+    #[pin(input)]
+    pub in1: bool,
+    #[pin(input)]
+    pub in2: bool,
+    #[pin(input)]
+    pub in3: bool,
+    #[pin(input)]
+    pub in4: bool,
+    #[pin(output)]
+    pub out: bool,
+}
+
+impl pcb_rs::Chip for Or4 {
+    fn tick(&mut self) {
+        self.out = self.in1 || self.in2 || self.in3 || self.in4;
+    }
+}
+
+#[derive(Chip, Default)]
+pub struct Or5 {
+    #[pin(input)]
+    pub in1: bool,
+    #[pin(input)]
+    pub in2: bool,
+    #[pin(input)]
+    pub in3: bool,
+    #[pin(input)]
+    pub in4: bool,
+    #[pin(input)]
+    pub in5: bool,
+    #[pin(output)]
+    pub out: bool,
+}
+
+impl pcb_rs::Chip for Or5 {
+    fn tick(&mut self) {
+        self.out = self.in1 || self.in2 || self.in3 || self.in4 || self.in5;
+    }
+}
